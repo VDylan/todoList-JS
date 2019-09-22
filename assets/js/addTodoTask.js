65794e4c38d7089fee5f8a3c7ac293b4e5dddd41
DylanV <dylanv.dev@gmail.com>
2019-09-22 12:26:27 +0200

    Add function to take the input value length in addTodoTask.js

--- assets/js/addTodoTask.js
+++ assets/js/addTodoTask.js
@@ -4,3 +4,7 @@ const button = document.querySelector("#submitButton");
 const input = document.querySelector("#inputValue");
 const list = document.querySelector("ul");
 let li;
+
+function inputLength() {
+  return input.value.length;
+}
